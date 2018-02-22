c1d9a46885f4d3acdc77f5b8977341903aaf2835
belleprincesse <g16l0585@ru.ac.za>
2018-02-22 17:07:01 +0200

    Prac2update

--- FSharpKoans/AboutLists.fs
+++ FSharpKoans/AboutLists.fs
@@ -3,21 +3,21 @@
 open NUnit.Framework
 
 (*
-Lists are immutable, ordered, finite sequences of a single type.
+Lists are immutable, ordered, finite sequences of a single type. compared to tuples that take multiple tuples
 *)
 
 module ``05: I Have Here In My Hand A List`` = 
     [<Test>]
     let ``01 Creating a list (Syntax 1).`` () = 
-        let myList = [ __; __; __; __ ]
+        let myList = ["apple"; "grape"; "pear"; "biscuit" ]
         myList |> should equal [ "apple"; "grape"; "pear"; "biscuit" ]
    
     [<Test>]
     let ``02 Creating a list (Syntax 2).`` () =
-        let myList = __::__::__::__::[]
-        let myOtherList = __::__::__::[ __ ]
-        let myNextList = __::__::__ // you may use [ and ] symbols on this line.
-        let myLastList = __::__::__ // DO NOT use [ or ] symbols on this line!
+        let myList = "apple"::"grape"::"pear"::"biscuit"::[]          //add start of the list, so the first data is apple
+        let myOtherList = "orange"::"lemon"::"princess"::["queen"]   // queen is already in the list, the first is orange then the last is queen
+        let myNextList = "lily"::"sunflower"::"daisy"::["carrot"] // you may use [ and ] symbols on this line.
+        let myLastList = "naartjie"::"raisin"::"apple"::"grape"::"pear"::"biscuit"::[] // DO NOT use [ or ] symbols on this line!
         myList |> should equal [ "apple"; "grape"; "pear"; "biscuit" ]
         myOtherList |> should equal [ "orange"; "lemon"; "princess"; "queen" ]
         myNextList |> should equal ["lily"; "sunflower"; "daisy"; "carrot"]
@@ -27,77 +27,77 @@ module ``05: I Have Here In My Hand A List`` =
     let ``03 Creating a list (via concatenation).`` () =
         let a = [902; 10]
         let b = [3; 13; 37]
-        let result = __ @ __
+        let result = a @ b    //order is important 
         result |> should equal [902; 10; 3; 13; 37]
 
     [<Test>]
-    let ``04 The : : symbol (called "cons") does not modify an existing list`` () = 
+    let ``04 The : : symbol (called "cons") does not modify an existing list`` () =  //cons adds to an existing list
         let first = [ "grape"; "peach" ]
         let second = "pear" :: first
         let third = "apple" :: second
-        third |> should equal __
-        second |> should equal __
-        first |> should equal __
+        third |> should equal ["apple"; "pear" ; "grape"; "peach" ] //cannot do this ["pear" :: first]
+        second |> should equal  [ "pear" ; "grape"; "peach" ]
+        first |> should equal [ "grape"; "peach" ]
 
     [<Test>]
     let ``05 Pattern-matching a list (Part 1).`` () =
-        let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
-        let a::_ = fruits
-        a |> should equal __
+        let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]  //list has a head and a tail
+        let a::_ = fruits                            // a and wildcard. Wildcard does not return anything. 
+        a |> should equal "apple"                    // a takes the head and the wildcard matches the rest of the list 
 
     [<Test>]
     let ``06 Pattern-matching a list (Part 2).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let b::c::_ = fruits
-        b |> should equal __
-        c |> should equal __
+        b |> should equal "apple"
+        c |> should equal "peach"
 
     [<Test>]
     let ``07 Pattern-matching a list (Part 3).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let _::d::e = fruits
-        d |> should equal __
-        e |> should equal __
+        d |> should equal "peach"     //take the head 
+        e |> should equal  [ "orange"; "watermelon"; "pineapple"; "tomato"]   //take the rest of the list
 
     [<Test>]
     let ``08 Pattern-matching a list (Part 4).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let f::_::_::g::_ = fruits
-        f |> should equal __
-        g |> should equal __
+        f |> should equal "apple"
+        g |> should equal "watermelon" // does not include the rest of the list because of the wildcard. 
 
     [<Test>]
     let ``09 Pattern-matching a list (Part 5).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let _::_::_::h = fruits
-        h |> should equal __
+        h |> should equal [ "watermelon"; "pineapple"; "tomato"]
 
     [<Test>]
     let ``10 Pattern-matching a list (Part 6).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let [i;j;k;l;m;n] = fruits
-        i |> should equal __
-        j |> should equal __
-        k |> should equal __
-        l |> should equal __
-        m |> should equal __
-        n |> should equal __
+        i |> should equal "apple"
+        j |> should equal "peach"
+        k |> should equal "orange"
+        l |> should equal "watermelon"
+        m |> should equal"pineapple"
+        n |> should equal  "tomato"
 
     [<Test>]
     let ``11 Pattern-matching a list (Part 7).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let _::o::_::[p;q;r] = fruits
-        o |> should equal __
-        p |> should equal __
-        q |> should equal __
-        r |> should equal __
+        o |> should equal  "peach"
+        p |> should equal "watermelon"
+        q |> should equal "pineapple"
+        r |> should equal  "tomato"
 
     [<Test>]
     let ``12 Pattern-matching a list (Part 8).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let [_;s;_;_;t;_] = fruits
-        s |> should equal __
-        t |> should equal __
+        s |> should equal "peach"
+        t |> should equal "pineapple"
 
     [<Test>]
     let ``13 Pattern-matching a list (Part 9).`` () =
@@ -107,11 +107,11 @@ module ``05: I Have Here In My Hand A List`` =
             | [a;b;c;d;e] -> "prune"
             | _::t::_::u::_ -> t + u
             | _ -> "fig"
-        k |> should equal __
+        k |> should equal "peachwatermelon"
 
     [<Test>]
     let ``14 Creating a list containing a sequence of numbers, and indexing`` () =
-        let k = [6..50]
-        let l = [3..3..20]
-        k.[3] |> should equal __
-        l.[3] |> should equal __
+        let k = [6..50]        // sequential till 50
+        let l = [3..3..20]    // sequence with a difference in 3 until 20
+        k.[3] |> should equal 9  //.[3] which index position that considers index 0  
+        l.[3] |> should equal 12
--- FSharpKoans/AboutTuples.fs
+++ FSharpKoans/AboutTuples.fs
@@ -13,41 +13,41 @@ module ``04: Tuples`` =
     [<Test>]
     let ``01 Creating tuples`` () = 
         let items = "apple", "dog"
-        items |> should equal ("apple", __)
+        items |> should equal ("apple", "dog" )
 
     [<Test>]
     let ``02 Elements of a tuple can be different types`` () =
-        let stuff = "Rivet", false, 22.5
-        stuff |> should equal ( __ )
+        let stuff = "Rivet", false, 22.5             // order is important  //same type and same value
+        stuff |> should equal ("Rivet", false, 22.5  )
 
     [<Test>]
     let ``03 Decompose a tuple using tuple pattern`` () =
         let aida = 2020, "cranberry", false, "wait, what?"
-        let a, b, c, d = aida
-        a |> should equal __
-        b |> should equal __
-        c |> should equal __
-        d |> should equal __
+        let a, b, c, d = aida                                   // identifiers are in the same order as the data structure
+        a |> should equal 2020
+        b |> should equal "cranberry"
+        c |> should equal false
+        d |> should equal "wait, what?"
 
     [<Test>]
     let ``04 Using a tuple in a match expression`` () =
         let result =
-            match "Teresa", "pasta" with
-            | name, "veggies" -> name + " likes vegetables"
+            match "Teresa", "pasta" with       //combination of different patterns like  match  x (identifier) % 20 (constant) = 0
+            | name, "veggies" -> name + " likes vegetables"   //combination of the above as well
             | name, "fish" -> name + " likes seafood"
             | name, "chicken" -> name + " crows about their food"
-            | FILL__ME_IN, FILL__ME_IN -> __ + " loves to eat " + __
-        result |> should equal "Teresa loves to eat pasta"   
+            | name, "pasta"  -> name + " loves to eat " + "pasta"  // output is not the same as matching
+        result |> should equal "Teresa loves to eat pasta"        //green line means possible match failure exception
    
     [<Test>]
     let ``05 The type of a tuple is the contained types separated by '*' symbols`` () =
         let a = 3, 5, "hi", 'x', 7.22
-        a |> should be ofType<FILL_ME_IN>
+        a |> should be ofType<int * int* string* char* float> //each type for each value
 
     [<Test>]
     let ``06 Using the wildcard pattern`` () = 
         // A single _ is a wildcard pattern, NOT a space to fill things in.
         // A wildcard pattern successfully matches ANYTHING.
         let name, _, _, weapon_name = "Shinji", 9103, true, "Unit 01"
-        name |> should equal __
-        weapon_name |> should equal __
+        name |> should equal "Shinji"
+        weapon_name |> should equal "Unit 01"
